456e09828e0637753629b6c0b3392685d6af942a
ch0taku1408-art <ch0taku1408@gmail.com>
2026-01-28 13:12:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from supabase import create_client
+
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(url, key)
 
 # -----------------------------
 # ページ設定
